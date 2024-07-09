6769e31600a6ea1a781d97a434cac382d838c49d
gustn1029 <gustn1029@gmail.com>
2024-07-09 10:39:13 +0900

    2024-07-09 boolean

---
+++ js/js/js_bool.js
@@ -0,0 +1,14 @@
+// truthy: boolean으로 타입을 변환했을 때, true가 되는 값
+// falsy: boolean으로 타입을 변환했을 때, false가 되는 값
+
+// truthy
+console.log(`true: ${!!true}`);
+console.log(`23: ${!!23}`);
+console.log(`hello: ${!!"hello"}`);
+
+// falsy
+console.log(`false: ${!!false}`);
+console.log(`0: ${!!0}`);
+console.log(`"": ${!!""}`);
+console.log(`null: ${!!null}`);
+console.log(`undefined: ${!!undefined}`);
