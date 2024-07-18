0f90927aaac6bcdf0b3cca808ed262bb077bcf28
gustn1029 <gustn1029@gmail.com>
2024-07-18 13:02:10 +0900

    docs

---
+++ docs/js/script.js
@@ -0,0 +1,15 @@
+const selectBtn = document.querySelector(".btn-select");
+const memberList = document.querySelector(".list-member");
+const selectMember = document.querySelector(".select-member");
+
+selectBtn.addEventListener('click', () => {
+    selectBtn.classList.toggle("on");
+    // memberList.classList.toggle("on");
+});
+
+memberList.addEventListener("click", (e)=> {
+    if(e.target.nodeName === "BUTTON") {
+        selectMember.textContent = `나의 최애 프로그래밍 언어는 ${e.target.textContent} 입니다.`;
+        selectBtn.classList.remove("on");
+    }
+})
