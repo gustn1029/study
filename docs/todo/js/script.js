6f3be07b7537d0d10b323741a8528272fc0ec0e5
gustn1029 <gustn1029@gmail.com>
2024-07-24 11:09:34 +0900

    todoList 기능 추가, 배열로 관리

--- docs/todo/js/script.js
+++ docs/todo/js/script.js
@@ -8,8 +8,13 @@ let todos = [];
 const addTodo = (e) => {
   e.preventDefault();
   if (todo.value !== "" || todo.value.length !== 0) {
-    todos.push(todo.value);
-    addListItem();
+    const todoData = {
+      id: todos.length + 1,
+      text: todo.value,
+      isComplete: false,
+    };
+    todos.push(todoData);
+    addListItem(todoData);
   } else {
     alert("할 일을 입력해주세요.");
   }
@@ -17,30 +22,85 @@ const addTodo = (e) => {
   todo.value = "";
 };
 
-const addListItem = () => {
+const addListItem = (todoData) => {
   const li = document.createElement("li");
   const p = document.createElement("p");
+  const div = document.createElement("div");
   const delBtn = document.createElement("button");
+  const updateBtn = document.createElement("button");
 
-  todos.forEach((el) => {
+  p.textContent = todoData.text;
+  delBtn.textContent = "삭제";
+  delBtn.classList.add("delete-btn");
+  updateBtn.textContent = "수정";
+  updateBtn.classList.add("update-btn");
 
-    p.textContent = el;
-    delBtn.textContent = "삭제";
-    delBtn.classList.add("delete-btn");
+  if (todoData.isComplete) {
+    p.classList.add("complete");
+  }
 
-    li.append(p);
-    li.append(delBtn);
+  li.id = todoData.id;
+  li.append(p);
+  div.append(updateBtn);
+  div.append(delBtn);
+  li.append(div);
 
-    todoList.appendChild(li);
-  });
+  todoList.appendChild(li);
 };
 
-const removeListItem = (e) => {
-  const val = e.target;
-  if(val.nodeName === "BUTTON") {
-    val.parentNode.remove();
+const updateTodo = (id, newText) => {
+  const updateItem = document.getElementById(id);
+  const updateText = updateItem.querySelector("p");
+
+  (newText !== "" && newText !== null)
+    ? (updateText.textContent = newText)
+    : updateText.textContent;
+};
+
+const completeTodo = (id, isComplete) => {
+  const completeTodoItem = document.getElementById(id);
+
+  const p = completeTodoItem.querySelector("p");
+  if (isComplete) {
+    p.classList.add("complete");
+    p.textContent += "(완료)";
+  } else {
+    p.classList.remove("complete");
   }
-}
-addListItem();
+};
+
+const removeTodo = (id) => {
+  const item = document.getElementById(id);
+  item.remove();
+};
+
+const setListItemStatus = (e) => {
+  const val = e.target;
+
+  if (val.nodeName === "P") {
+    const id = Number(val.parentNode.id);
+    const confirm = window.confirm("완료 처리 하시겠습니까?");
+    if (confirm) {
+      todos.map((el) => (el.id === id ? { ...el, isComplete: true } : el));
+      completeTodo(id, true);
+    }
+  } else if (val.nodeName === "BUTTON") {
+    const id = Number(val.parentNode.parentNode.id);
+    if (val.classList.contains("delete-btn")) {
+      todos = todos.filter((el) => el.id !== id);
+      removeTodo(id);
+    }
+
+    if (val.classList.contains("update-btn")) {
+      const newText = window.prompt(
+        "새로운 내용을 입력해주세요:",
+        val.parentNode.previousElementSibling.textContent
+      );
+      updateTodo(id, newText);
+    }
+  }
+  console.log(todos);
+};
+
 addBtn.addEventListener("click", addTodo);
-todoList.addEventListener("click", removeListItem)
+todoList.addEventListener("click", setListItemStatus);
